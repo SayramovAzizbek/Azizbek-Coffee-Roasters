86ef748243cb39c668e77ff3324b9ff0ff0b2b1d
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-06-27 19:11:59 +0500

    Add modal window

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var modalOpenBtn = document.querySelector('.modal-open-btn')
+var modalMain = document.querySelector('.modal-main')
+var closeModal = document.querySelector('.close-modal-btn')
+
+modalOpenBtn.addEventListener('click', () => {
+    modalMain.classList.add('modal-main-On')
+})
+
+closeModal.addEventListener('click', () => {
+modalMain.classList.remove('modal-main-On')
+})
